634fe66b188732fd31ddff3a260a89caf44aab77
FlEtOsAuRoM99 <mariotti.samuele1999@gmail.com>
2024-01-04 12:14:38 +0100

    manage a post

--- ForumGames/Post/views.py
+++ ForumGames/Post/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect
-
+from .forms import WritePost
 # Create your views here.
 
+def postInsert(request):
+    form = WritePost(request.POST or None)
+
+    return render(request=request,
+                  template_name="html/ForumGames/Blog.html",
+                  context={"form":form})
