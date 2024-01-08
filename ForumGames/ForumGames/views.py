07dc561674297e9a3c180f5d3c88451c1f65e64e
FlEtOsAuRoM99 <mariotti.samuele1999@gmail.com>
2024-01-08 17:05:57 +0100

    add Videogame and post

--- ForumGames/ForumGames/views.py
+++ ForumGames/ForumGames/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render, redirect
 from Computer.models import ComponentsComputer
 from Post.forms import WritePost
+from Post.models import Post
 def home(request):
 
     photo = elPC = post = None
-    if request.user.id != None:
-        l = ComponentsComputer.objects.filter(nickname_id=request.user.id).all()
-        if not l.exists():
+    if request.user.is_authenticated:
+        components= ComponentsComputer.objects.filter(nickname_id=request.user.id).all()
+
+        if not components.exists():
             return redirect("Computer:createComputer")
         else:
+            post_formatting = Post.objects.filter(user_id=request.user.id).all()
             elPC = []
-            for i in range(len(l)):
-                elPC.append("CPU: " + l[i].CPU)
-                elPC.append("GPU: " + l[i].GPU)
-                elPC.append("RAM: " + l[i].RAM)
-                elPC.append("MEM: " + l[i].MEM)
-                elPC.append("PSU: " + l[i].PSU)
-                elPC.append("MOBO: " + l[i].MOBO)
-                photo = l[i].PC_Photo
+            photo = []
+            for i in range(len(components)):
+                elPC.append("CPU: " + components[i].CPU)
+                elPC.append("GPU: " + components[i].GPU)
+                elPC.append("RAM: " + components[i].RAM)
+                elPC.append("MEM: " + components[i].MEM)
+                elPC.append("PSU: " + components[i].PSU)
+                elPC.append("MOBO: " + components[i].MOBO)
+                elPC.append(components[i].PC_Photo)    
         post = WritePost(request.POST or None)
     ctx={"elenco": elPC, "ph": photo, "form": post}
 
